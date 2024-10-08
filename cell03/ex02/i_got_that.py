cdadf494ecc68187c884a2e467e4ca49a3b19f6f
iconpyr <iconpiyorod@gmail.com>
2024-10-08 13:51:59 +0700

    Update i_got_that.py

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
 x = str(input('What you gotta say? : '))
 
 while x != "":
-    x = str(input("I got that! Anythinf else : "))
+    x = str(input("I got that! Anything else : "))
 
     if x == "STOP":
         break
